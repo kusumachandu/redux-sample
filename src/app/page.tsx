ca2f6f7f49dfd343959049205127944b93f27e88
chandra shekar <kusumachandu43@gmail.com>
2023-08-06 23:55:24 +0530

    ✨added the handlechange function to the input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
+"use client"
+
 import Input from '@/components/Input'
-import Image from 'next/image'
+import { useRef, useState } from 'react'
 
 export default function Home() {
+  const [value, setValue] = useState('');
+
+  const handleInputChange = (value: string) => {
+    console.log(value);
+    setValue(value);
+  }
+
   return (
     <>
       <h1 className='uppercase font-bold text-slate-100 text-2xl text-center py-2'>Sample-redux</h1>
       <section className='w-1/2 mx-auto py-10'>
-        <Input placeholder='Enter the todo list here' />
+        <Input
+          value={value}
+          onChange={handleInputChange}
+          placeholder='Enter the todo list here' 
+        />
       </section>
     </>
   )
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,26 @@
+"use client"
 import React, { FC } from 'react';
 import { cn } from '@/lib/utils';
 
-export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  value: string;
+  onChange: (value: any) => void;
+}
 
 
-const Input: FC<InputProps> = ({ className, ...props}, ref) => {
+const Input: FC<InputProps> = ({ className, value, onChange, ...props}, ref) => {
+  function handleChange(event: any){
+    onChange(event?.target.value)
+  }
   return <input
+  ref={ref}
   className={cn(
     'flex h-10 w-full rounded-md border border-slate-300 bg-transparent py-2 px-3 text-sm placeholder:text-slate-400 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-700 dark:text-slate-50 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900',
     className
   )}
-  ref={ref}
+  type='text'
+  value={value}
+  onChange={handleChange}
   {...props}
 />
 }
