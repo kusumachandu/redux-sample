936a6da724a6c5c5a27f34e36ca92edc607d9938
chandra shekar <kusumachandu43@gmail.com>
2023-08-08 21:39:07 +0530

    ✨created card component for todo list

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
 }
 
 const Button: FC<ButtonProps> = ({ children, className, onClick, ...props}) => {
-  return <button onClick={onClick} className={cn('px-10 py-1 bg-gray-500 text-slate-100 uppercase rounded-lg text-center', className)} {...props} >{children}</button>
+  return <button onClick={onClick} className={cn('px-8 py-1 bg-gray-500 text-slate-100 uppercase rounded-lg text-center', className)} {...props} >{children}</button>
 }
 
 export default Button
---
+++ src/components/Card.tsx
@@ -0,0 +1,23 @@
+import { FC, useContext } from 'react'
+import MyContext from '@/context/MyContext'
+import Button from './Button';
+
+interface CardProps {}
+
+const Card: FC<CardProps> = ({}) => {
+  const data = useContext(MyContext);
+  return <div className='w-1/3 mx-auto'>
+    {
+      data?.map((todo: any, index: number) => (
+        <li key={index} className='border list-none py-2 px-2 rounded-lg shadow-sm shadow-white flex justify-between items-center'>
+          {todo}
+          <div>
+            <Button className='bg-red-400 font-bold' onClick={() => {}}>delete</Button>
+          </div>
+        </li>
+      ))
+    }
+  </div>
+}
+
+export default Card
