25fd95917dfaf874cdf773307d4418a2f7ef699a
chandra shekar <kusumachandu43@gmail.com>
2023-08-11 22:55:03 +0530

    ✨added the popup functionality

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -1,11 +1,26 @@
 import { FC } from 'react'
+import Input from './Input'
+import Button from './Button';
 
 interface PopUpProps {
-  
+  setIsOpen: (value: any) => void;
+  isOpen: boolean;
 }
 
-const PopUp: FC<PopUpProps> = ({}) => {
-  return <div>PopUp</div>
+const PopUp: FC<PopUpProps> = ({setIsOpen, isOpen}) => {
+  function closePopUp() {
+    setIsOpen(!isOpen);
+  }
+  return <div className=''>
+    <Input
+      value='edit'
+      onChange={() => {}}
+      placeholder='Enter the todo list here' 
+    />
+    <div>
+      <Button className='uppercase' onClick={closePopUp}>close</Button>
+    </div>
+  </div>
 }
 
 export default PopUp
