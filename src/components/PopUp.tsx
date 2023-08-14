49fe86d8024e953ad80390b34ba894483c00ab55
chandra shekar <kusumachandu43@gmail.com>
2023-08-14 23:13:07 +0530

    🚧working on editing functionality of todo list

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -1,20 +1,29 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Input from './Input'
 import Button from './Button';
 
 interface PopUpProps {
+  todo: any;
   setIsOpen: (value: any) => void;
   isOpen: boolean;
 }
 
-const PopUp: FC<PopUpProps> = ({setIsOpen, isOpen}) => {
+const PopUp: FC<PopUpProps> = ({todo, setIsOpen, isOpen}) => {
+
+  console.log(todo)
+  const [editedTodo, setEditedTodo] = useState(todo);
+
   function closePopUp() {
     setIsOpen(!isOpen);
   }
+
+  function handleTodoEdit(event: any) {
+    setEditedTodo(event.target.value);
+  }
   return <div className='w-1/2 py-32 flex flex-col gap-4'>
     <Input
-      value='Edit'
-      onChange={() => {}}
+      value={editedTodo}
+      onChange={handleTodoEdit}
       placeholder='Enter the todo list here' 
       className=' mx-auto'
     />
