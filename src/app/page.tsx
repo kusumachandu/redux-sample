22333c3204bdb458262053407015398b01b77913
chandra shekar <kusumachandu43@gmail.com>
2023-09-07 10:29:26 +0530

    🚧redux integration under progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 "use client"
 
-export default function Home() {
- 
+import { useSelector, useDispatch } from "react-redux/es/exports";
+import { increment, decrement } from "@/redux/actions";
 
+export default function Home() {
+
+  // const count = useSelector<any>((state) => state);
+  // const dispatch = useDispatch();
   return (
     <>
       <h1 className='uppercase font-bold text-slate-100 text-2xl text-center py-2'>Sample-redux Revoked</h1>
+      {/* <p>Count: {count}</p> */}
     </>
   )
 }
